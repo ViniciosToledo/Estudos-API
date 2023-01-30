3aebd61b62a4cbab06d7726a843802448538b770
Vinicios Toledo Abolafio <viabolafio@hotmail.com>
2023-01-29 21:05:23 -0300

    Estudando a função fetch e tentando consumir dados da API de Rick and Morty para treinar

--- src/components/Pensando.jsx
+++ src/components/Pensando.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
 const Pensando = () => {
+
+  const API = 'https://rickandmortyapi.com/api/'
+  const characters = []
+
+  fetch(API+'character')
+  .then(async function(res) {
+    
+    characters = await res.json().results
+    
+  })
+  .catch(function(err) {
+    console.log(err)
+  });
+
+  console.log(characters)
+
   return (
     <div>
-        <h1>Ola</h1>
+
     </div>
   )
 }
